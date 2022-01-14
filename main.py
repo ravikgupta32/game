5581616f57364c821021f4b4006f19f1dfdccb1e
Ravi Kumar Gupta <ravikgupta32@gmail.com>
2022-01-14 22:31:04 +0530

    commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from turtle import Screen
+import pygame
+#intialize the pygame
+pygame.init()
+
+
+#create the screen
+screen = pygame.display.set_mode((800,600))
+#Title and Icon
+pygame.display.set_caption("Warriors")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+#Game Loop
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    screen.fill(())        
